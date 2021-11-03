219da9481e87e35f92a174e2a92d6d543002eb38
bzab076 <bzab076@gmail.com>
2021-11-03 22:54:21 +0100

    Initial commit

---
+++ src/main/kotlin/AbstractDay.kt
@@ -0,0 +1,21 @@
+import java.io.File
+
+abstract class AbstractDay (private val dayNumber: Int) {
+
+    abstract fun partOne(): Number
+
+    abstract fun partTwo(): Number
+
+    fun inputLines(): List<String> {
+
+        return File(javaClass.classLoader.getResource("day${dayNumber.toString().padStart(2, '0')}.txt").toURI()).readLines()
+
+    }
+
+    fun inputString(): String {
+
+        return File(javaClass.classLoader.getResource("day${dayNumber.toString().padStart(2, '0')}.txt").toURI()).readText()
+
+    }
+
+}
---
+++ src/main/kotlin/Utils.kt
@@ -0,0 +1,4 @@
+
+fun <T, U> cartesianProduct(c1: Collection<T>, c2: Collection<U>): List<Pair<T, U>> {
+    return c1.flatMap { lhsElem -> c2.map { rhsElem -> lhsElem to rhsElem } }
+}
