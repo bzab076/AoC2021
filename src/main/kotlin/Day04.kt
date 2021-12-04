045a75585ed9f54f24d38bb7064ceadb3accf264
bzab076 <bzab076@gmail.com>
2021-12-04 08:20:07 +0100

    day 4

---
+++ src/main/kotlin/Day04.kt
@@ -0,0 +1,116 @@
+class Day04 : AbstractDay(4) {
+
+    private var numbers : List<Int> = emptyList()
+    private var boards : MutableList<MutableList<Pair<Int,Boolean>>> = emptyList<MutableList<Pair<Int,Boolean>>>().toMutableList()
+
+    override fun partOne(): Number {
+
+        parseInput()
+
+        // play game
+        for(num in numbers) {
+
+            for(board in boards) {
+
+                for(i in board.indices) {
+                    if(board[i].first == num) {
+                        board[i] = Pair(num,true)
+                    }
+                }
+
+                if(isBoardComplete(board)) {
+                    println("Winning board index ${boards.indexOf(board)}")
+                    println("Winning number is ${num}")
+
+                    val unmarked = board.filter { it -> !it.second }.map { it -> it.first }.sum()
+                    return unmarked*num
+                }
+            }
+
+        }
+
+        return 0 // should not happen
+    }
+
+    override fun partTwo(): Number {
+
+        numbers = emptyList()
+        boards  = emptyList<MutableList<Pair<Int,Boolean>>>().toMutableList()
+        parseInput()
+
+        val winningSet : MutableSet<Int> = emptySet<Int>().toMutableSet()
+
+        // play game
+        for(num in numbers) {
+
+            for(board in boards) {
+
+                for(i in board.indices) {
+                    if(board[i].first == num) {
+                        board[i] = Pair(num,true)
+                    }
+                }
+
+                if(isBoardComplete(board)) {
+
+                    winningSet.add(boards.indexOf(board))
+                    if(winningSet.size == boards.size) {
+                        println("Last winning board index " + boards.indexOf(board))
+                        println("Last winning number is ${num}")
+
+                        val unmarked = board.filter { it -> !it.second }.map { it -> it.first }.sum()
+                        return unmarked*num
+                    }
+
+                }
+            }
+
+        }
+
+        return 0 // should not happen
+    }
+
+    private fun parseInput() {
+
+        val rawInput = inputLines()
+        numbers = rawInput.first().split(",").map{it -> it.toInt()}
+
+        var currentBoard : MutableList<Pair<Int,Boolean>> = emptyList<Pair<Int,Boolean>>().toMutableList()
+
+        for(line in rawInput.drop(2)) {
+
+            if(line.trim().isEmpty()) {
+                boards.add(currentBoard)
+                currentBoard = emptyList<Pair<Int,Boolean>>().toMutableList()
+                continue
+            }
+
+            val boardLine = line.split(" ").filter { it -> !it.isEmpty() }.map { it -> Pair(it.toInt(), false) }
+            for(elem in boardLine)
+                currentBoard.add(elem)
+        }
+
+        boards.add(currentBoard)
+    }
+
+    private fun isBoardComplete(board : List<Pair<Int,Boolean>>) : Boolean {
+
+        var lineComplete = false
+        for(i in board.indices) {
+            if(lineComplete && i % 5 == 0) break
+            if(i % 5 == 0) lineComplete = board[i].second
+            lineComplete = lineComplete && board[i].second
+        }
+
+        var columnComplete = false
+        for (c in 0..4) {
+            for(i in 0..4) {
+                if(columnComplete && i == 0) break
+                if(i == 0) columnComplete = board[c + i*5].second
+                columnComplete = columnComplete && board[c + i*5].second
+            }
+        }
+
+        return lineComplete || columnComplete
+    }
+}
