84d0ada9114a3920bf0efd0df93709c51cd863c3
bzab076 <bzab076@gmail.com>
2021-12-06 18:10:25 +0100

    day 6

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,54 @@
+class Day06 : AbstractDay(6) {
+
+    private val results : MutableMap<Int, Long> = emptyMap<Int,Long>().toMutableMap()
+
+    override fun partOne(): Number {
+
+        // get number of descendants for every fish in the list and sum them into the result
+        return inputString().split(",").map { it -> countFishOptimized(it.toInt(), 80) }.sum()
+    }
+
+    override fun partTwo(): Number {
+
+        // get number of descendants for every fish in the list and sum them into the result
+        return inputString().split(",").map { it -> countFishOptimized(it.toInt(), 256) }.sum()
+    }
+
+
+    /*
+    * Counts fish with initial timer value and all its descendants
+     */
+    private fun countFish(timer : Int, startDay: Int, endDay : Int) : Long {
+
+        var fishCount = 1L
+        var fishNumber = timer
+
+        for(d in startDay..endDay) {
+            if(fishNumber == 0) {
+                fishNumber = 6
+                fishCount += countFish(8,d+1,endDay)
+            }
+            else {
+                fishNumber--
+            }
+        }
+
+        return fishCount
+    }
+
+    /*
+    * Optimized fish count where already counted values are stored in a map and reused later.
+     */
+    private fun countFishOptimized(timer : Int, endDay : Int) : Long {
+
+        val key = timer + endDay
+
+        if(!results.containsKey(key)) {
+            results.put(key, countFish(timer, 1, endDay))
+        }
+
+        val res = results.get(key)
+        return if (res != null) res else 0
+
+    }
+}
