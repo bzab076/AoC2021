8c02bcd3b8411dc031ffd61476ee7a2e45113e2c
bzab076 <bzab076@gmail.com>
2021-12-10 08:33:59 +0100

    day 10

---
+++ src/main/kotlin/Day10.kt
@@ -0,0 +1,79 @@
+class Day10 : AbstractDay(10) {
+
+    override fun partOne(): Number {
+
+        return inputLines().sumOf { getCorruptionScore(it) }
+    }
+
+    override fun partTwo(): Number {
+
+        val scores = inputLines().filter { getCorruptionScore(it) == 0 }.map { getCompletionScore(it) }.sorted()
+        return scores[(scores.size - 1) / 2]
+    }
+
+    private fun getCorruptionScore(line : String) : Int {
+
+        var corruptionScore = 0
+        val openBrackets : MutableList<Char> = emptyList<Char>().toMutableList()
+
+        line.forEach {
+
+            if(listOf<Char>('[','{','(','<').contains(it)) {
+                openBrackets.add(it)
+            }
+            else {
+                when (it) {
+                    ')' -> if(openBrackets.last() != '(')  corruptionScore = 3
+                    ']' -> if(openBrackets.last() != '[')  corruptionScore = 57
+                    '}' -> if(openBrackets.last() != '{')  corruptionScore = 1197
+                    '>' -> if(openBrackets.last() != '<')  corruptionScore = 25137
+                }
+                openBrackets.removeLast()
+            }
+
+            if(corruptionScore>0) {
+                return corruptionScore
+            }
+        }
+
+        return 0 // if line is not corrupted we come to here
+    }
+
+    private fun getCompletionScore(line : String) : Long {
+
+        val openBrackets : MutableList<Char> = emptyList<Char>().toMutableList()
+
+        line.forEach {
+            if(listOf<Char>('[','{','(','<').contains(it)) {
+                openBrackets.add(it)
+            }
+            else {
+                openBrackets.removeLast()
+            }
+        }
+
+        val closingBrackets = openBrackets.reversed().map{ it ->
+            when (it) {
+                '(' -> ')'
+                '[' -> ']'
+                '{' -> '}'
+                '<' -> '>'
+                else -> '.' // must not happen
+            }
+        }
+
+        val score = closingBrackets.fold(0L) { acc, it ->
+            val charScore: Int
+            when (it) {
+                ')' -> charScore=1
+                ']' -> charScore=2
+                '}' -> charScore=3
+                '>' -> charScore=4
+                else -> charScore=0
+            }
+            acc*5 + charScore
+        }
+
+        return score
+    }
+}
